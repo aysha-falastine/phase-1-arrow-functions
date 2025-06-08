9ee895e812945d978138139ac1ec686558e6ee1e
asha mohamed <falastine924@gmail.com>
2025-06-08 21:08:33 +0300

    index.html

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression
+const divide = function(a, b) {
+  return a / b;
+};
+
+// Arrow function that squares a number
+const square = x => x * x;
+
+// Arrow function that adds two numbers
+const add = (a, b) => a + b;
